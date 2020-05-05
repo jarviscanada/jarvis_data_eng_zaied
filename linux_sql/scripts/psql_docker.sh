6a4ce5bd9427684e847566aa7ae5b9965dc4f17b
zaied54 <zzaman2@.uwo.ca>
2020-05-05 20:32:42 +0000

    fixedall the errors from code review

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -5,10 +5,7 @@ command=$1
 username=$2
 password=$3
 
-if [[ ! ( $"(systemctl status docker)" || $"(systemctl start docker)") ]]; then
-    echo "Daemon is not running"
-    exit 1
-fi
+systemctl status docker || systemctl start docker
 if [[ $command = "create" ]]; then
     if [[ $(docker container ls -a -f name=jrvs-psql | wc -l) -ge 2 ]]; then 
         echo "jrvis-psql container is already created"
@@ -20,7 +17,7 @@ if [[ $command = "create" ]]; then
     fi
     docker volume create pgdata
     docker run --name jrvs-psql -e POSTGRES_PASSWORD=$password -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 $username
-    exit 0
+    exit $?
 fi
 if [[ ! $(docker ps -a | grep "jrvs-psql") ]]; then 
    echo "jrvis-psql container is not created yet. Usage: ./scripts/psql_docker.sh create db_username db_password"
@@ -29,13 +26,12 @@ fi
 if [[ $command = "start" ]]; then
    docker container start jrvs-psql
    #echo "Successfully started"
-   exit 0
+   exit $?
 fi
 if [[ $command = "stop" ]]; then
-   docker container start jrvs-psql
    docker container stop jrvs-psql
    #echo "Successfully stopped"
-   exit 0
+   exit $?
 fi
 echo "Invalid command. Usage: ./scripts/psql_docker.sh create db_username db_password or start or stop"
 exit 1
