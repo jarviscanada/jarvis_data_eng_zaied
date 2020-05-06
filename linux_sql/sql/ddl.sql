66e77a8b9a6aefd6a8259dba8f6c112a3ad1ce5c
zaied54 <zzaman2@.uwo.ca>
2020-05-06 21:26:37 +0000

    minor fix

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS PUBLIC.host_info
 	id SERIAL PRIMARY KEY NOT NULL,
 	hostname VARCHAR NOT NULL,
 	cpu_number INTEGER NOT NULL,
-	cpu_architecture INTEGER NOT NULL,
+	cpu_architecture VARCHAR NOT NULL,
 	cpu_model VARCHAR NOT NULL,
 	cpu_mhz float8 NOT NULL,
 	L2_cache INTEGER NOT NULL,
