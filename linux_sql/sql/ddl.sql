5e587a7369b721607bf65e690293a35579bd4ca1
zaied54 <zzaman2@.uwo.ca>
2020-05-07 21:00:52 +0000

    no need

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS PUBLIC.host_info 
 	(
 	id SERIAL PRIMARY KEY NOT NULL,
-	hostname VARCHAR NOT NULL,
+	hostname VARCHAR UNIQUE NOT NULL,
 	cpu_number INTEGER NOT NULL,
-	cpu_architecture INTEGER NOT NULL,
+	cpu_architecture VARCHAR  NOT NULL,
 	cpu_model VARCHAR NOT NULL,
 	cpu_mhz float8 NOT NULL,
 	L2_cache INTEGER NOT NULL,
