59a2cef74f4dd716ce7dc5e7f10a26faa54fbdbf
zaied54 <zzaman2@uwo.ca>
2020-07-04 19:39:47 -0400

    quotecontroller

--- springboot/src/main/java/ca/jrvs/apps/trading/AppConfig.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/AppConfig.java
@@ -1,4 +1,4 @@
 package ca.jrvs.apps.trading;
 
-public class Application {
+public class AppConfig {
 }
---
+++ springboot/src/test/java/ca/jrvs/apps/trading/TestConfig.java
@@ -0,0 +1,44 @@
+import ca.jrvs.apps.trading.model.domain.MarketDataConfig;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.http.conn.HttpClientConnectionManager;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan(basePackages = {"ca.jrvs.apps.trading.dao","ca.jrvs.apps.trading.service"})
+public class TestConfig {
+
+    @Bean
+    public MarketDataConfig marketDataConfig(){
+        MarketDataConfig marketDataConfig = new MarketDataConfig();
+        marketDataConfig.setHost("https://cloud.iexapis.com/v1/");
+        marketDataConfig.setToken("pk_7d126699746b47089ffc453a938c1eb3");
+        return marketDataConfig;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        String url = System.getenv("PSQL_URL");
+        String user = System.getenv("PSQL_USER");
+        String password = System.getenv("PSQL_PASSWORD");
+        //Never log your credentials/secrets. Use debugger instead
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(url);
+        basicDataSource.setUsername(user);
+        basicDataSource.setPassword(password);
+        return basicDataSource;
+    }
+
+    @Bean
+    public HttpClientConnectionManager httpClientConnectionManager() {
+        PoolingHttpClientConnectionManager poolingHttpClientConnectionManager = new PoolingHttpClientConnectionManager();
+        poolingHttpClientConnectionManager.setMaxTotal(50);
+        poolingHttpClientConnectionManager.setDefaultMaxPerRoute(50);
+        return poolingHttpClientConnectionManager;
+    }
+}
