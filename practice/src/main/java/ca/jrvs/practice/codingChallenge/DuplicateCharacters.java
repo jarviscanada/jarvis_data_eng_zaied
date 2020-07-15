2beba863397f1261ba5760ffdd653d5dc3613f33
zaied54 <zzaman2@uwo.ca>
2020-07-15 19:24:13 -0400

    impl

---
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/DuplicateCharacters.java
@@ -0,0 +1,38 @@
+package ca.jrvs.practice.codingChallenge;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * AUTHOR: zaied
+ * DATE: 2020-07-15
+ * TIME: 5:40 p.m.
+ **/
+
+public class DuplicateCharacters {
+
+    public static List<Character> duplicateCharacters(String str)
+    {
+        List<Character> res = new ArrayList<>();
+        int[] mark = new int[58];
+        Arrays.fill(mark,0);
+        for(int i=0;i<str.length();i++)
+        {
+            char ch = str.charAt(i);
+            if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+            {
+                if(mark[ch - 'A'] == 1)
+                {
+                    mark[ch - 'A']++;
+                    res.add(ch);
+                }
+                else
+                {
+                    mark[ch-'A']++;
+                }
+            }
+        }
+        return res;
+    }
+}
